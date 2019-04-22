fdd064bd93c53b3b9c08a3d626fd03ae7880a214
Андрей <nowosad@inbox.ru>
2019-04-22 14:14:35 +0400

    Add project files.

---
+++ RoingCustomersDb.UI/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace RoingCustomersDb.UI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
