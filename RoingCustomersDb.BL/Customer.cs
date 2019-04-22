4e463aad94438109be02b22e9be8afb5b57ba846
Андрей <nowosad@inbox.ru>
2019-04-22 14:33:11 +0400

    Добавлены проекты для слоев приложения

---
+++ RoingCustomersDb.BL/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RoingCustomersDb.BL
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime? Birthdate { get; set; }
+    }
+}
---
+++ RoingCustomersDb.BL/ICustomerRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace RoingCustomersDb.BL
+{
+    public interface ICustomerRepository
+    {
+        IEnumerable<Customer> GetCustomers();
+        Customer GetCustomerById(int id);
+        void AddCustomer(Customer customer);
+        void UpdateCustromer(Customer customer);
+        void DeleteCustomer(Customer customer);
+    }
+}
