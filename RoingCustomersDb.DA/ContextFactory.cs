18d04654f5e5a4a71a9412695b3f0cf80bd13341
Андрей <nowosad@inbox.ru>
2019-04-22 15:01:57 +0400

    Добавлена реализация фабрики контекстов EF

---
+++ RoingCustomersDb.DA/ContextFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity.Infrastructure;
+
+namespace RoingCustomersDb.DA
+{
+    public class ContextFactory : IDbContextFactory<Context>
+    {
+        public Context Create() => new Context();
+    }
+}
