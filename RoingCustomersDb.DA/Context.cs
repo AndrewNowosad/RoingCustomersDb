40bd8afb762c3f4022451a2d18d55a465188403b
Андрей <nowosad@inbox.ru>
2019-04-23 10:10:24 +0400

    Добавлен конфиг в проект UI. Установлены инициализаторы БД.

--- RoingCustomersDb.DA/Context.cs
+++ RoingCustomersDb.DA/Context.cs
@@ -5,6 +5,12 @@ namespace RoingCustomersDb.DA
 {
     public class Context : DbContext
     {
+        static Context()
+        {
+            Database.SetInitializer(new CreateDatabaseIfNotExists<Context>());
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<Context>());
+        }
+
         public Context() : base("name=Context") { }
 
         public DbSet<Customer> Customers { get; set; }
