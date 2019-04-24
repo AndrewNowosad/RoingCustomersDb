c55ab831e3c947f929c42a7c36811133c31a1321
Андрей <nowosad@inbox.ru>
2019-04-24 15:48:34 +0400

    Доработки по GUI

--- RoingCustomersDb.UI/App.xaml.cs
+++ RoingCustomersDb.UI/App.xaml.cs
@@ -1,11 +1,20 @@
 ﻿using RoingCustomersDb.DA;
 using RoingCustomersDb.UI.ViewModels;
+using System.Globalization;
 using System.Windows;
+using System.Windows.Markup;
 
 namespace RoingCustomersDb.UI
 {
     public partial class App : Application
     {
+        public App()
+        {
+            FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement),
+                new FrameworkPropertyMetadata(XmlLanguage.GetLanguage(
+                    CultureInfo.CurrentUICulture.IetfLanguageTag)));
+        }
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
