cab2447ea2cd6f490f7e90476efdf4666f6bfe1a
Андрей <nowosad@inbox.ru>
2019-04-24 10:47:51 +0400

    Добавлена реализация простой команды

---
+++ RoingCustomersDb.UI/SimpleCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace RoingCustomersDb.UI
+{
+    class SimpleCommand : ICommand
+    {
+        protected readonly Func<bool> _canExecute;
+        protected readonly Action _execute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public SimpleCommand(Action execute)
+            : this(execute, () => true) { }
+
+        public SimpleCommand(Action execute, Func<bool> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute ?? throw new ArgumentNullException(nameof(canExecute));
+        }
+
+        public bool CanExecute(object parameter) => _canExecute();
+        public void Execute(object parameter) => _execute();
+
+        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
